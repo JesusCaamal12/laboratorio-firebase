b9efaaa0899628aaa2426aa886d76690e55be4c9
JesusCaamal12 <caamalnohjesus@gmail.com>
2025-05-01 14:39:55 -0600

    Key

--- database/firebase.ts
+++ database/firebase.ts
@@ -1,14 +1,32 @@
+import Constants from "expo-constants";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Verificar si expoConfig está disponible
+const config = Constants.expoConfig?.extra;
+
+if (!config) {
+  throw new Error("Firebase configuration is missing in expoConfig.extra");
+}
+
+const {
+  firebaseApiKey,
+  firebaseAuthDomain,
+  firebaseProjectId,
+  firebaseStorageBucket,
+  firebaseMessagingSenderId,
+  firebaseAppId,
+  firebaseMeasurementId
+} = config;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDfoaYgzVjeN5_Ka8ent9r-4LaG-UlYya8",
-  authDomain: "base-sensores-d256e.firebaseapp.com",
-  projectId: "base-sensores-d256e",
-  storageBucket: "base-sensores-d256e.firebasestorage.app",
-  messagingSenderId: "533360119827",
-  appId: "1:533360119827:web:2bcb25bf30f8f594f1efe6",
-  measurementId: "G-5HQJV98S0P"
+  apiKey: firebaseApiKey,
+  authDomain: firebaseAuthDomain,
+  projectId: firebaseProjectId,
+  storageBucket: firebaseStorageBucket,
+  messagingSenderId: firebaseMessagingSenderId,
+  appId: firebaseAppId,
+  measurementId: firebaseMeasurementId
 };
 
 const app = initializeApp(firebaseConfig);
